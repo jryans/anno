f0866cccbbf1c92dc9f129bbaf462c112e5fa694
J. Ryan Stinnett <jryans@gmail.com>
2023-07-31 18:18:24 +0100

    Add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    /// File to annotate
+    file: PathBuf,
+    /// Add an annotation data producer
+    #[arg(short, long = "producer", id = "PRODUCER")]
+    producers: Vec<String>,
+    /// Add an annotation data source
+    #[arg(short, long = "source", id = "SOURCE")]
+    sources: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{:?}", cli);
 }
