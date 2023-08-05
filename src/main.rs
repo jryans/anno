ced3a166848cd737e523f1697c37a23e885ea442
J. Ryan Stinnett <jryans@gmail.com>
2023-08-05 21:28:14 +0100

    Collect produced annotations and display them

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::path::PathBuf;
+use std::{
+    cmp::max,
+    path::PathBuf,
+    str::{FromStr, Lines},
+};
 
-use anyhow::{Context, Ok, Result};
+use anyhow::{Context, Error, Ok, Result};
 use clap::Parser;
+use duct::cmd;
 use log::debug;
 use url::Url;
 
@@ -21,7 +26,7 @@ struct Cli {
     /// - Producer with data source and additional arguments
     ///   `producer:/path/to/data/source?param=value`
     #[arg(short, long = "producer", id = "PRODUCER", verbatim_doc_comment)]
-    producers: Vec<Url>,
+    producers: Vec<Producer>,
 
     #[command(flatten)]
     verbose: clap_verbosity_flag::Verbosity,
@@ -35,7 +40,7 @@ fn main() -> Result<()> {
         .filter_level(cli.verbose.log_level_filter())
         .init();
 
-    debug!("{:?}", cli);
+    debug!("CLI: {:?}", cli);
 
     if cli.producers.is_empty() {
         eprintln!("Warning: No producers, displaying file without annotations");
@@ -48,15 +53,78 @@ fn main() -> Result<()> {
             cli.file.display()
         )
     })?;
-    let line_count = target_content.lines().count();
-    debug!("Lines: {}", line_count);
+    let target_line_count = target_content.lines().count();
+    debug!("Lines: {}", target_line_count);
 
-    // TODO: Run producers
+    // Collect output from each producer
+    // TODO: Run in parallel
+    let mut produced_annotations = Vec::new();
+    for producer in cli.producers {
+        debug!("Producer: {:?}", producer);
+        let command_name = format!("anno-{}", producer.name());
+        let mut command = cmd!(&command_name);
+        command = command.env("ANNO_PRODUCER", producer.name());
+        command = command.env("ANNO_SOURCE", producer.source());
+        command = command.env("ANNO_LINES", target_line_count.to_string());
+        let data = command
+            .read()
+            .with_context(|| format!("Annotation producer `{}` failed", &command_name))?;
+        let max_width = data.lines().fold(0, |acc, line| max(acc, line.len()));
+        // For initial basic annotation format, ensure we have an annotation for every target line
+        assert!(data.lines().count() == target_line_count);
+        let annotations = Annotations { data, max_width };
+        debug!("Annotations: {:?}", annotations);
+        produced_annotations.push(annotations);
+    }
 
     // Write file content with annotations added
+    let mut produced_table: Vec<(Lines, usize)> = produced_annotations
+        .iter()
+        .map(|a| (a.data.lines(), a.max_width))
+        .collect();
     for line in target_content.lines() {
+        for produced_lines in &mut produced_table {
+            print!(
+                "{:width$} | ",
+                produced_lines.0.next().unwrap(),
+                width = produced_lines.1
+            );
+        }
         println!("{}", line);
     }
 
     Ok(())
+}
+
+#[derive(Clone, Debug)]
+struct Producer(Url);
+
+impl Producer {
+    fn parse(input: &str) -> Result<Producer> {
+        Ok(Producer(Url::parse(input)?))
+    }
+
+    fn name(&self) -> &str {
+        self.0.scheme()
+    }
+
+    fn source(&self) -> &str {
+        self.0.path()
+    }
+}
+
+impl FromStr for Producer {
+    type Err = Error;
+
+    fn from_str(input: &str) -> std::result::Result<Self, Self::Err> {
+        Producer::parse(input)
+    }
+}
+
+#[derive(Debug)]
+struct Annotations {
+    /// Raw data from the annotation producer
+    data: String,
+    /// Maximum width of `data` across all lines
+    max_width: usize,
 }
